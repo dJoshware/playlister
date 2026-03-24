a4c5d36d41c467c2d9141c0196857341c8f08a46
Joshua Ellis <xiixxxixcvi@gmail.com>
2026-03-24 14:32:26 -0500

    Add catch for any Spotify CDN subdomains for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     allowedDevOrigins: ['192.168.1.214'],
+    images: {
+        remotePatterns: [
+            { protocol: 'https', hostname: 'i.scdn.co' },
+            { protocol: 'https', hostname: 'mosaic.scdn.co' },
+            { protocol: 'https', hostname: 'image-cdn-ak.spotifycdn.com' },
+            { protocol: 'https', hostname: 'image-cdn-fa.spotifycdn.com' },
+            { protocol: 'https', hostname: 'thisis-images.scdn.co' },
+            { protocol: 'https', hostname: 'lineup-images.scdn.co' },
+            { protocol: 'https', hostname: '*.spotifycdn.com' },
+            { protocol: 'https', hostname: '*.scdn.co' },
+        ],
+    },
 };
 
 export default nextConfig;
